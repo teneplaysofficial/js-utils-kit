c0bf4c1bab521cc51321a7493a6dbcb96d52e585
Sriman <136729116+TenEplaysOfficial@users.noreply.github.com>
2025-06-24 11:27:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS Utils Kit
+
+A compact library of essential JavaScript utility functions—such as array operations, object helpers, Promise utilities, and type checks. Designed to be lightweight, dependable, and effortlessly integrated into JS or TS projects, it has minimal bundle impact and works seamlessly with modern bundlers.
---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import del from 'rollup-plugin-delete';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import pkg from './package.json' assert { type: 'json' };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: false },
+      { file: pkg.module, format: 'esm', sourcemap: false },
+    ],
+    plugins: [
+      json(),
+      del({ targets: ['dist/*'], runOnce: true }),
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+    ],
+    external: [...Object.keys(pkg.peerDependencies || {})],
+  },
+];
