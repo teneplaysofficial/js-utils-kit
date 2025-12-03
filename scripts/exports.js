8b60885f4800dcb0db6a01dc8e4ac880839e7b9c
Sriman <136729116+teneplaysofficial@users.noreply.github.com>
2025-12-03 16:51:28 +0530

    fix

--- scripts/exports.js
+++ scripts/exports.js
@@ -7,13 +7,17 @@ for await (const dir of glob('packages/*')) {
   const output = createWriteStream(join(dir, 'src', 'index.ts'));
   const pkg = JSON.parse(await readFile(join(dir, 'package.json'), 'utf-8'));
 
-  for (const [dep] of Object.entries(pkg.dependencies ?? {})) {
+  for (const [dep] of Object.entries(pkg.dependencies ?? {}).sort(([a], [b]) =>
+    a.localeCompare(b),
+  )) {
     if (dep.startsWith('@js-utils-kit/')) {
       output.write(`export * from '${dep}';` + EOL);
     }
   }
 
-  for await (const file of glob(`${dir}/src/*`, { exclude: ['**/index.ts'] })) {
+  for (const file of (
+    await Array.fromAsync(glob(`${dir}/src/*`, { exclude: ['**/index.ts'] }))
+  ).sort((a, b) => a.localeCompare(b))) {
     output.write(`export * from './${basename(file, '.ts')}';` + EOL);
   }
 
