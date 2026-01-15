c7e5a4e31542181c094f63ad4435edd0a4774048
Sriman <136729116+teneplaysofficial@users.noreply.github.com>
2026-01-15 10:44:25 +0530

    add compact

---
+++ .changeset/ninety-wasps-camp.md
@@ -0,0 +1,5 @@
+---
+'@js-utils-kit/array': minor
+---
+
+Add `compact` utility to remove falsy values from arrays.
---
+++ packages/array/src/compact.ts
@@ -0,0 +1,24 @@
+/**
+ * Removes all falsy values from an array.
+ *
+ * Falsy values include: `false`, `null`, `0`, `""`, `undefined`, and `NaN`.
+ *
+ * @returns A new array with all falsy values removed
+ *
+ * @example
+ * ```ts
+ * compact([0, 1, false, 2, '', 3, null]);
+ * // [1, 2, 3]
+ *
+ * compact(['a', '', 'b', undefined]);
+ * // ['a', 'b']
+ * ```
+ */
+export function compact<T>(
+  /**
+   * A list of elements to compact
+   */
+  array: readonly T[],
+) {
+  return array.filter(Boolean);
+}
