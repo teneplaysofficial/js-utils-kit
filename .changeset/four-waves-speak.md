b956d01d8bda81752e204ddcda5dfecd7b1b98ad
Sriman <136729116+teneplaysofficial@users.noreply.github.com>
2026-03-29 02:27:00 +0530

    feat: add new fs utils

---
+++ .changeset/four-waves-speak.md
@@ -0,0 +1,5 @@
+---
+'@js-utils-kit/system': patch
+---
+
+Add fallback value for `CPUModel` when CPU info is unavailable
--- packages/@js-utils-kit/fs/src/index.ts
+++ packages/@js-utils-kit/fs/src/index.ts
@@ -6,5 +6,6 @@
 export * from './createArchive';
 export * from './env';
 export * from './file';
+export * from './json';
 export * from './locator';
 export * from './path';
